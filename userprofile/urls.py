1e08f0b9a28cfe7ea3860983b1618524f215fb9d
Oscar Parkin-Christie <oscarp.c@outlook.com>
2024-02-07 15:38:58 +0000

    Added Toast User Feedback and Error Handling for Profile Page

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    # Profile | Update Profile Details
+    path('update_name/', views.update_name, name='update_name'),
+    path('update_phone/', views.update_phone, name='update_phone'),
+    path('update_address/', views.update_address, name='update_address'),
+    path('update_license/', views.update_license, name='update_license'),
+    path('update_theory_test/', views.update_theory_test, name='update_theory_test'),
+    path('update_practical_test/', views.update_practical_test, name='update_practical_test'),
     path('lessons/', views.lessons, name='lessons'),
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.utils.safestring import mark_safe
 from django.utils import timezone
 from django.db import models
+
+from django.db.models import Q
 
 from django.conf import settings
 
@@ -42,66 +45,96 @@ def profile(request):
     Displays toast/s to prompt user to finish filling in their profile.
     """
     if (
-        hasattr(request.user, 'username') and request.user.username # Checks if user has a username
-        and hasattr(request.user, 'email') and request.user.email # Checks if user has an email address
-        and all(# Checks if user profile is not complete
-                not getattr(profile, field.name) 
-                for field in profile._meta.fields 
-                if (
-                    field.name != 'id'
-                    and field.name != 'user'
-                    and field.name != 'theory_test_date'
-                    and field.name != 'theory_test_center'
-                    and field.name != 'mock_test_date'
-                    and field.name != 'practical_test_date'
-                    and field.name != 'practical_test_center'
-                )
-            )
+    hasattr(request.user, 'username') and request.user.username and
+    hasattr(request.user, 'email') and request.user.email
     ):
-        messages.info(request, "Please finish setting up your profile.")
+        if not any(
+            getattr(profile, field.name)
+            for field in profile._meta.fields
+            if field.name not in [
+                'id', 'user',
+                'theory_test_date', 'theory_test_center',
+                'mock_test_date',
+                'practical_test_date', 'practical_test_center',
+            ]
+        ):
+            messages.info(request, "Please finish setting up your profile.")
+        else:
+            incomplete_fields = []
+            if not profile.first_name:
+                incomplete_fields.append("First Name")
+            if not profile.last_name:
+                incomplete_fields.append("Last Name")
+            if not profile.phone:
+                incomplete_fields.append("Phone No.")
+            if not profile.license_no:
+                incomplete_fields.append("Provisional License No.")
+            if not profile.license_expiry:
+                incomplete_fields.append("Provisional License's Expiry Date")
+            if not any(
+                getattr(profile, field_name)
+                for field_name in [
+                    'home_house_no',
+                    'home_street',
+                    'home_town',
+                    'home_post_code',
+                ]
+            ):
+                incomplete_fields.append("Full Home Address")
+
+            if incomplete_fields:
+                if len(incomplete_fields) > 1:
+                    incomplete_fields_html = ''.join(
+                        [
+                            f'<li class="list-group-item">{field}</li>'
+                            for field in incomplete_fields
+                            ]
+                    )
+                    message = mark_safe(
+                        f'<i class="fa-solid fa-circle-info"></i> \
+                            <span class="fs-6"> \
+                                Please provide the following information: \
+                                    </span> \
+                                        <ul class="list-group \
+                                            list-group-numbered \
+                                                list-group-flush"> \
+                                                    {incomplete_fields_html} \
+                                                        </ul>')
+                    messages.info(request, message)
+                else:
+                    incomplete_fields_html = ''.join(
+                        [
+                            f'<li class="list-group-item">{field}</li>' 
+                            for field in incomplete_fields
+                            ]
+                    )
+                    message = mark_safe(
+                        f'<i class="fa-solid fa-circle-info"></i> \
+                            <span class="fs-6"> \
+                                Please provide the following information: \
+                                    </span> \
+                                        <ul class="list-group \
+                                                list-group-flush"> \
+                                                    {incomplete_fields_html} \
+                                                        </ul>')
+                    messages.info(request, message)
+    else:
+        messages.error(
+            request,
+            "Hmm, that's odd. \
+                Your profile was not assigned a username or email address \
+                    upon registration. Please contact us \
+                        and we'll get this fixed."
+        )
 
 
 
-    """
-    if not profile.first_name or not profile.last_name:
-        messages.info(request, "You haven't told us your name")
-    elif not profile.phone:
-        messages.info(request, "Please provide your phone number. \
-            We might need it if we need to get in touch on the day \
-                of your lesson.")
-    elif not profile.license_no:
-        messages.info(request, "Please provide your \
-            provisional license number. \
-                Your instructor will need to verify your identity \
-                    before you hit the road.")
-    elif not profile.license_expiry:
-        messages.info(request, "Please provide your \
-            provisional license's expiry date. \
-                We need to check it is valid.")
-    elif (
-        not profile.home_house_no
-        or not profile.home_street
-        or not profile.home_town
-        or not profile.home_post_code
-    ):
-        messages.error(request, "Please finish filling out your \
-            home address details.")
-    elif not user.username:
-        messages.error(request, "Hmm, that's odd. \
-            It seems your username wasn't saved \
-            to your profile when you registered. \
-                Please contact us and we'll get this fixed.")
-    elif not user.email:
-        messages.error(request, "Hmm, that's odd. \
-            It seems your email address wasn't saved \
-            to your profile when you registered. \
-                Please contact us and we'll get this fixed.")
-    """
 
     # Booking Info:
 
     """ Gets all bookings made by and associated with the user """
-    all_bookings = profile.bookings.all().order_by('lesson_date', 'lesson_time')
+    all_bookings = profile.bookings.all().order_by(
+        'lesson_date', 'lesson_time')
 
     """
     Gets all bookings with a 'lesson_date' that is
@@ -122,7 +155,12 @@ def profile(request):
     def find_mock_test_booking(all_bookings):
         for booking in all_bookings:
             if booking.lesson_type == "Mock Test":
-                return booking.lesson_date, booking.lesson_time, booking.house_no, booking.post_code
+                return (
+                    booking.lesson_date,
+                    booking.lesson_time,
+                    booking.house_no,
+                    booking.post_code
+                )
     mock_test_booking = find_mock_test_booking(all_bookings)
 
 
@@ -142,6 +180,125 @@ def profile(request):
     return render(request, template, context)
 
 
+""" Update Details Form Handling """
+# Name
+def update_name(request):
+    if request.method == 'POST':
+        profile = get_object_or_404(UserProfile, user=request.user)
+        form = UpdateNameForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Name updated successfully')
+            return redirect('profile')
+        else:
+            form = UpdateNameForm(instance=profile)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'userprofile/profile.html', context)
+
+
+# Phone Number
+def update_phone(request):
+    if request.method == 'POST':
+        profile = get_object_or_404(UserProfile, user=request.user)
+        form = UpdatePhoneNumberForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Phone number updated successfully')
+            return redirect('profile')
+        else:
+            form = UpdatePhoneNumberForm(instance=profile)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'userprofile/profile.html', context)
+
+
+# Home Address
+def update_address(request):
+    if request.method == 'POST':
+        profile = get_object_or_404(UserProfile, user=request.user)
+        form = UpdateHomeAddressForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Home address updated successfully')
+            return redirect('profile')
+        else:
+            form = UpdateHomeAddressForm(instance=profile)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'userprofile/profile.html', context)
+
+
+
+# License
+def update_license(request):
+    if request.method == 'POST':
+        profile = get_object_or_404(UserProfile, user=request.user)
+        form = UpdateLicenseForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Provisional license details \
+                 updated successfully')
+            return redirect('profile')
+        else:
+            form = UpdateLicenseForm(instance=profile)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'userprofile/profile.html', context)
+
+
+# Theory Test
+def update_theory_test(request):
+    if request.method == 'POST':
+        profile = get_object_or_404(UserProfile, user=request.user)
+        form = UpdateTheoryTestForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Theory test details \
+                updated successfully')
+            return redirect('profile')
+        else:
+            form = UpdateTheoryTestForm(instance=profile)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'userprofile/profile.html', context)
+
+
+# Practical Test
+def update_practical_test(request):
+    if request.method == 'POST':
+        profile = get_object_or_404(UserProfile, user=request.user)
+        form = UpdatePracticalTestForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Practical test details \
+                updated successfully')
+            return redirect('profile')
+        else:
+            form = UpdateTheoryTestForm(instance=profile)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'userprofile/profile.html', context)
+
+
 def lessons(request):
     """ Gets all objects associated with the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -149,7 +306,10 @@ def lessons(request):
     now = timezone.now()
 
     """ Gets all bookings made by and associated with the user. """
-    all_bookings = profile.bookings.all().order_by('lesson_date', 'lesson_time')
+    all_bookings = profile.bookings.all().order_by(
+        'lesson_date',
+        'lesson_time'
+    )
 
     """
     Gets all bookings with a 'lesson_date' that is
@@ -170,7 +330,10 @@ def lessons(request):
     # Descending Order (Newest to Oldest) displays oldest lessons at bottom
     previous_bookings = profile.bookings.filter(
         models.Q(lesson_date__lt=timezone.now().date()) |
-        models.Q(lesson_date=timezone.now().date(), lesson_time__lt=timezone.now().time())
+        models.Q(
+            lesson_date=timezone.now().date(),
+            lesson_time__lt=timezone.now().time()
+        )
     ).order_by('-lesson_date', '-lesson_time')
 
     
