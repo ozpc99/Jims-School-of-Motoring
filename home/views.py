cc79f1ef315e090cc8ace54c3fe7424aa3652bca
Oscar Parkin-Christie <oscarp.c@outlook.com>
2024-02-13 13:26:06 +0000

    Home Page Views 5
    Added conditional check if user is authenticated before trying to access profile var

--- home/views.py
+++ home/views.py
@@ -6,14 +6,16 @@ from django.contrib.auth.models import User
 from userprofile.models import UserProfile
 
 def index(request):
-    user = request.user
-    """ Gets all objects associated with the user's profile. """
-    profile = get_object_or_404(UserProfile, user=request.user)
+    # Checks if user is authenticated
+    if request.user.is_authenticated:
+        profile = get_object_or_404(UserProfile, user=request.user)
+    else:
+        profile = None
 
     """ A view to return the index page """
     template = 'home/index.html'
     context = {
-        'user': user,
+        'user': request.user,
         'profile': profile,
     }
 
