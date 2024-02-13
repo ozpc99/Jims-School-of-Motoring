6a6e3c795f81805a2064bd3ee376915229e01b05
Oscar Parkin-Christie <oscarp.c@outlook.com>
2024-02-13 15:22:02 +0000

    Reverted to previous MEDIA_URL and changed logo img href to direct link to S3 object

--- jims_school_of_motoring/settings.py
+++ jims_school_of_motoring/settings.py
@@ -177,7 +177,7 @@ if 'USE_AWS' in os.environ:
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/{MEDIAFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
