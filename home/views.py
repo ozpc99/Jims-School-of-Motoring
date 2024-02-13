48be4861d4fa765611deedb3b338f959e09146e6
Oscar Parkin-Christie <oscarp.c@outlook.com>
2024-02-13 13:04:22 +0000

    Home page view conditional check 3

--- home/views.py
+++ home/views.py
@@ -4,17 +4,8 @@ from django.conf import settings
 from userprofile.models import UserProfile
 
 def index(request):
-    username = request.user.username
-    print("Username:", username)
-    
-    if request.user.is_authenticated:
-        profile = get_object_or_404(UserProfile, user=request.user)
-
     """ A view to return the index page """
     template = 'home/index.html'
-    context = {
-        'username': username,
-        'profile': profile,
-    }
+    context = {}
 
     return render(request, template, context)
